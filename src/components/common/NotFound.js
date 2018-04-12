d9ceb9cfa3c8fd0138053fb90ba99efb6b96efa2
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2018-04-12 15:06:30 +0100

    Added 404 page with a cute lil link

--- src/components/common/NotFound.js
+++ src/components/common/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+
 const NotFound = () => {
   return(
     <div className="container">
-      <h1 className="error-title"> Error 404: The page you&apos;re looking for can&apos;t be found. </h1>
+      <div className="error-container">
+        <h1 className="error-title"> Error 404: The page you&apos;re looking for cannot be found. </h1>
+        <p className="error-subtitle">We&apos;ve sent out a search party but if you&apos;re still feeling lost, click the taxi to take you home.</p>
+        <br />
+        <Link to="/challenges"> <img className="taxi" src="../assets/images/taxi.jpeg"/> </Link>
+      </div>
     </div>
   );
 };
